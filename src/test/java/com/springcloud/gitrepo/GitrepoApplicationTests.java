94a0e41e52f1e5dfe9e25c1118afc7d2a878766a
sirisha annamneedi <sirisha.annamneedi@servicenow.com>
2025-01-23 13:56:24 +0530

    sample push

---
+++ src/test/java/com/springcloud/gitrepo/GitrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.gitrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
